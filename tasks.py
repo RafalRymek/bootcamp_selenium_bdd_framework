0daf1a2449071c024ae66d8c6be7b25d117c2587
rafal.rymek <rafal.rymek@brainly.com>
2021-06-08 13:28:44 +0200

    add allure reporting

--- tasks.py
+++ tasks.py
@@ -14,10 +14,12 @@ def run(context, browser="", tags="", resolution=""):
 
 
 @task
-def run_with_allure(context, browser="", tags=""):
+def run_with_allure(context, browser="", tags="", resolution=""):
     behave_cmd = "behave -f allure_behave.formatter:AllureFormatter -o artifacts -f pretty"
     if browser != "":
         behave_cmd = f"{behave_cmd} -D browser={browser}"
     if tags != "":
         behave_cmd = f"{behave_cmd} --tags={tags}"
+    if resolution != "":
+        behave_cmd = f"{behave_cmd} -D resolution={resolution}"
     context.run(behave_cmd)
